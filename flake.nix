aadd90ebc298a07c87988e7b81cc11ad430d048f
Michael Strobel <mstrobel97@gmail.com>
2025-02-21 08:32:07 +0100

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -11,50 +11,60 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    plenary-nvim,
-    ...
-  }: let
-    plugin-overlay = final: prev: {
-      plenary-nvim = (final.pkgs.vimUtils.buildVimPlugin {
-        name = "plenary.nvim";
-        src = plenary-nvim;
-      }).overrideAttrs {
-        nvimSkipModule = [
-          "plenary._meta._luassert"
-          "plenary.neorocks.init"
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      plenary-nvim,
+      ...
+    }:
+    let
+      plugin-overlay = final: prev: {
+        plenary-nvim =
+          (final.pkgs.vimUtils.buildVimPlugin {
+            name = "plenary.nvim";
+            src = plenary-nvim;
+          }).overrideAttrs
+            {
+              nvimSkipModule = [
+                "plenary._meta._luassert"
+                "plenary.neorocks.init"
+              ];
+            };
+
+        kanban-nvim =
+          (final.pkgs.vimUtils.buildVimPlugin {
+            name = "kanban.nvim";
+            src = self;
+          }).overrideAttrs
+            {
+              dependencies = [ final.plenary-nvim ];
+            };
       };
 
-      kanban-nvim = (final.pkgs.vimUtils.buildVimPlugin {
-        name = "kanban.nvim";
-        src = self;
-      }).overrideAttrs {
-        dependencies = [ final.plenary-nvim ];
-      };
-    };
-
-    supportedSystems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-  in
-    flake-utils.lib.eachSystem supportedSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          plugin-overlay
-        ];
-      };
-    in {
-      packages = rec {
-        default = kanban-nvim;
-        inherit (pkgs) kanban-nvim;
-      };
-    })
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            plugin-overlay
+          ];
+        };
+      in
+      {
+        packages = rec {
+          default = kanban-nvim;
+          inherit (pkgs) kanban-nvim;
+        };
+      }
+    )
     // {
       overlays.default = plugin-overlay;
     };
